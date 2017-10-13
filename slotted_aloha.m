b86af4f7a04f564ca9a4cf90befe0da8e933ef6d
hedin94 <94rasmushedin@gmail.com>
2017-10-13 21:37:23 +0200

    Add more comments to the code

--- slotted_aloha.m
+++ slotted_aloha.m
@@ -1,36 +1,46 @@
 function [backlog, arrival, departure] = slotted_aloha(m, T, lambda, qr)
 
-% State of the nodes
+% State of a nodes
 % 0: Idle
 % 1: Transmitting
 % 2: Backlogged
 state = zeros(1,m);
+
 transmission_slot = zeros(1,m);
 backlog = zeros(1,T);
 arrival = zeros(1,T);
 departure = zeros(1,T);
-meanDelay = 0;
+arrival_slot = zeros(1,T);
+departure_slot = zeros(1,T);
 arrivedPckts = 0;
 transmPckts = 0;
+
+% Calculate the probability of arrival
 qa = 1 - exp(-lambda/m);
 
+% Simulate for T slots
 t = 0;
 while t < T
     t = t + 1;
-    
+
     for i = 1:m
+        % Is node_i IDLE?
         if state(i) == 0
-            % Arrival at node i?
+            % Packet arrive at node_i with probability qa
             if rand() <= qa
                 state(i) = 1;
                 transmission_slot(i) = t;
                 arrivedPckts = arrivedPckts + 1;
+                arrival_slot(arrivedPckts) = t; 
             end
         else
-            % Retransmission at node i?
+            % Node i is backlogged
+            % Retransmit with probability qr
             if rand() <= qr
                 state(i) = 1;
             else
+                % Node_i did not retransmit
+                % and stays backlogged
                 state(i) = 2;
                 backlog(t) = backlog(t) + 1;
             end
@@ -51,12 +61,27 @@ while t < T
     if transmissions == 1
         state(transmitting_node) = 0;
         transmPckts = transmPckts + 1;
-        delay = t - transmission_slot(transmitting_node);
-        meanDelay = meanDelay + (1/transmPckts)*(delay - meanDelay);
+        departure_slot(transmPckts) = t;
     end
     
+    % Store packet arrivals and departures
+    % at time slot t
     arrival(t) = arrivedPckts;
     departure(t) = transmPckts;
 end
 
-fprintf('\nqa: %.3f,\nTransmitted packets: %u,\nMean delay: %.0f\n',qa,transmPckts,meanDelay);
+% Calculate mean delay
+W = departure_slot(1:transmPckts) - arrival_slot(1:transmPckts);
+W = mean(W);
+
+fprintf('\n=========================\n')
+fprintf('Simulation finished\n')
+fprintf('Nodes: %u\n',m);
+fprintf('Slots %u\n',T);
+fprintf('lambda: %.3f\n',lambda);
+fprintf('qr: %.3f\n',qr);
+fprintf('qa: %.3f\n',qa);
+fprintf('Arrived packets: %u\n',arrivedPckts);
+fprintf('Transmitted packets: %u\n',transmPckts);
+fprintf('Mean delay: %.3f\n',W);
+fprintf('=========================\n')
--- stabilized_slotted_aloha.m
+++ stabilized_slotted_aloha.m
@@ -1,10 +1,11 @@
 function [backlog, backlog_estimate, arrival, departure, W] = stabilized_slotted_aloha(m, T, lambda)
 
-% State of the nodes
+% State of a nodes
 % 0: Idle
 % 1: Transmitting
 % 2: Backlogged
 state = zeros(1,m);
+
 transmission_slot = zeros(1,m);
 backlog = zeros(1,T);
 backlog_estimate = zeros(1,T+1);
@@ -12,12 +13,13 @@ arrival = zeros(1,T);
 departure = zeros(1,T);
 arrival_slot = zeros(1,T);
 departure_slot = zeros(1,T);
-meanDelay = 0;
 arrivedPckts = 0;
 transmPckts = 0;
+
+% Calculate the probability of arrival
 qa = 1 - exp(-lambda/m);
 
-
+% Simulate for T slots
 t = 0;
 while t < T
     t = t + 1;
@@ -30,8 +32,9 @@ while t < T
     end
     
     for i = 1:m
+        % Is node_i IDLE?
         if state(i) == 0
-            % Arrival at node i?
+            % Packet arrive at node_i with probability qa
             if rand() <= qa
                 state(i) = 2;
                 transmission_slot(i) = t;
@@ -40,10 +43,13 @@ while t < T
                 backlog(t) = backlog(t) + 1;
             end
         else            
-            % Retransmission at node i?
+            % Node i is backlogged
+            % Retransmit with probability qr
             if rand() <= qr
                 state(i) = 1;
             else
+                % Node_i did not retransmit
+                % and stays backlogged
                 state(i) = 2;
                 backlog(t) = backlog(t) + 1;
             end
@@ -65,8 +71,6 @@ while t < T
         state(transmitting_node) = 0;
         transmPckts = transmPckts + 1;
         departure_slot(transmPckts) = t;
-        delay = t - arrival_slot(transmPckts);
-        meanDelay = meanDelay + (1/transmPckts)*(delay - meanDelay);
     end
     
     % Estimate backlog for slot t+1
@@ -76,11 +80,23 @@ while t < T
         backlog_estimate(t+1) = backlog_estimate(t) + lambda + 1/(exp(1)-2);
     end
     
+    % Store packet arrivals and departures
+    % at time slot t
     arrival(t) = arrivedPckts;
     departure(t) = transmPckts;
 end
 
+% Calculate mean delay
 W = departure_slot(1:transmPckts) - arrival_slot(1:transmPckts);
 W = mean(W);
 
-fprintf('\nqa: %.3f,\nTransmitted packets: %u,\nMean delay: %.0f\n',qa,transmPckts,W);
+fprintf('\n=========================\n')
+fprintf('Simulation finished\n')
+fprintf('Nodes: %u\n',m);
+fprintf('Slots %u\n',T);
+fprintf('lambda: %.3f\n',lambda);
+fprintf('qa: %.3f\n',qa);
+fprintf('Arrived packets: %u\n',arrivedPckts);
+fprintf('Transmitted packets: %u\n',transmPckts);
+fprintf('Mean delay: %.3f\n',W);
+fprintf('=========================\n')
